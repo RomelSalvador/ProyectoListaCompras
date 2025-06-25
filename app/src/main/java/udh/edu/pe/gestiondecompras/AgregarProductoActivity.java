2a84c5c54e4b305f8b2d14aa33363f24a266b131
Stiven <2021110357@udh.edu.pe>
2025-06-25 12:18:44 -0500

    implementacion de spinner en productos con categoria

--- app/src/main/java/udh/edu/pe/gestiondecompras/AgregarProductoActivity.java
+++ app/src/main/java/udh/edu/pe/gestiondecompras/AgregarProductoActivity.java
@@ -3,7 +3,9 @@ package udh.edu.pe.gestiondecompras;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +13,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class AgregarProductoActivity extends AppCompatActivity {
 
     private EditText etNombre, etCantidad;
+    private Spinner spinnerCategoria;
     private int imagenSeleccionada = R.drawable.ic_launcher_foreground;
 
     @Override
@@ -20,11 +23,19 @@ public class AgregarProductoActivity extends AppCompatActivity {
 
         etNombre = findViewById(R.id.etNombre);
         etCantidad = findViewById(R.id.etCantidad);
+        spinnerCategoria = findViewById(R.id.spinnerCategoria);
+
+
+        String[] categorias = {"supermercado", "Limpieza", "farmacia"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, categorias);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCategoria.setAdapter(adapter);
     }
 
     public void guardarProducto(View view) {
         String nombre = etNombre.getText().toString().trim();
         String cantidadStr = etCantidad.getText().toString().trim();
+        String categoriaSeleccionada = spinnerCategoria.getSelectedItem().toString();
 
         if (nombre.isEmpty() || cantidadStr.isEmpty()) {
             Toast.makeText(this, "Complete todos los campos", Toast.LENGTH_SHORT).show();
@@ -32,14 +43,11 @@ public class AgregarProductoActivity extends AppCompatActivity {
             int cantidad = Integer.parseInt(cantidadStr);
 
 
-
-
-
-
             Intent intent = new Intent(this, ListaProductosActivity.class);
             intent.putExtra("nombre", nombre);
             intent.putExtra("cantidad", cantidad);
             intent.putExtra("poster", imagenSeleccionada);
+            intent.putExtra("categoria", categoriaSeleccionada);
             startActivity(intent);
         }
     }
