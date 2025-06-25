b521481b1b9e13332336bd8e67769cd3c1503ae8
Stiven <2021110357@udh.edu.pe>
2025-06-25 14:32:19 -0500

    implementacion de la clase Usuario

---
+++ app/src/main/java/udh/edu/pe/gestiondecompras/Usuario.java
@@ -0,0 +1,37 @@
+package udh.edu.pe.gestiondecompras;
+
+public class Usuario {
+    private String nombre;
+    private String correo;
+    private String contrasena;
+
+    public Usuario(String nombre, String correo, String contrasena) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.contrasena = contrasena;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+}
